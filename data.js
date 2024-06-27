83558c0100d0bada177001f098a32b970e7f7db1
RUBENGAENS <12101888@student.pxl.be>
2024-06-27 13:45:44 +0200

    updating portfolio

---
+++ data.js
@@ -0,0 +1,32 @@
+let songs = [
+    {
+        name: 'AERFQUAKE',
+        path: 'music/song1.mp3',
+        artist: 'Tylor, The Creator',
+        cover: 'images/cover 1.jpeg'
+    },
+    {
+        name: 'Glue',
+        path: 'music/song2.mp3',
+        artist: 'BICEP',
+        cover: 'images/cover 2.png'
+    },
+    {
+        name: 'Redbone',
+        path: 'music/song3.mp3',
+        artist: 'Childish Gambino',
+        cover: 'images/cover 3.png'
+    },
+    {
+        name: 'Feel Good Inc.',
+        path: 'music/song4.mp3',
+        artist: 'Gorillaz',
+        cover: 'images/cover 4.png'
+    },
+    {
+        name: 'The Less I Know The Better',
+        path: 'music/song5.mp3',
+        artist: 'Tame Impala',
+        cover: 'images/cover 5.png'
+    },
+]
