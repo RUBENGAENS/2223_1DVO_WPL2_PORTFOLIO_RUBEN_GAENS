9388e47866d67b727424e9e4f61a9c1c93720c0e
RUBENGAENS <12101888@student.pxl.be>
2024-10-12 11:27:21 +0200

    deleted aos animations

--- main.js
+++ main.js
@@ -59,3 +59,9 @@ function toggleNav() {
   }
 }
 
+document.addEventListener("DOMContentLoaded", function() {
+  const lazyImages = document.querySelectorAll("img[loading='lazy']");
+  lazyImages.forEach((img) => {
+      img.src = img.dataset.src;
+  });
+});
